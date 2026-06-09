ca5879b30984118014dab3e062da470338523ac6
Yashpal Gawali <gawaliyashpal@gmail.com>
2026-06-09 11:09:54 +0530

    9 Jun sec change

--- src/Components/Services/apiClient.js
+++ src/Components/Services/apiClient.js
@@ -1,8 +1,20 @@
 import axios from "axios";
 
+
+// For Local Development
+// export const apiClient = axios.create({
+//     baseURL: "http://localhost:9191/qrbarcodegenerator",
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+// })
+
+// For Local Development
 export const apiClient = axios.create({
-    baseURL: "http://localhost:9191/qrbarcodegenerator",
+    baseURL: "http://192.168.0.219:8081/qrbarcodegenerator",
     headers: {
         "Content-Type": "application/json",
     },
 })
+
+
